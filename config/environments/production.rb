852e329c7a805bc4f06461c629b612d852d5587b
Vinson Koo <vinsonkoo@gmail.com>
2015-07-26 14:31:16 -0400

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,10 +76,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.assets.precompile << Proc.new { |path|
-    if path =~ /\.(eot|svg|ttf|woff)\z/
-      true
-    end
-  }
 end
