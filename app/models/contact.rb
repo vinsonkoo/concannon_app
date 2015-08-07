ddda16a968465ad14969549e140e3308ce6c4090
Vinson Koo <vinsonkoo@gmail.com>
2015-08-06 22:38:34 -0400

    css changes

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -9,7 +9,7 @@ class Contact < MailForm::Base
   # in ActionMailer accepts.
   def headers
     {
-      :subject => "My Contact Form",
+      :subject => "Concannon Contact Form",
       :to => "vinson@hindsitemetrics.com",
       :from => %("#{name}" <#{email}>)
     }
